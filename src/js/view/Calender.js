1759aad4b7608d1a2d2557e6e67bd6966ed2cff4
戸塚孝高 <y047aka@totsukayoshitakanoMac-mini.local>
2019-02-14 20:40:05 +0900

    Display date in table-cells

--- src/js/view/Calender.js
+++ src/js/view/Calender.js
@@ -24,7 +24,7 @@ const TableBody = series => state =>
   tr([
     td(series.seriesName),
     sundays.map(d => ([
-      check(d, series) ? td({ class: 'raceweek', 'data-tooltip': '24 Hours of Le Mans' }, '') : td('')
+      check(d, series) ? td({ class: 'raceweek', 'data-tooltip': '24 Hours of Le Mans' }, d.getDate()) : td(d.getDate())
     ]))
   ])
 
