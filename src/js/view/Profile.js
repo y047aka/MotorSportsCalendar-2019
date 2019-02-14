ac9ff6fbfd83113e395850f689e3268541e94dc3
戸塚孝高 <y047aka@totsukayoshitakanoMac-mini.local>
2019-02-14 22:43:17 +0900

    Update style

--- src/js/view/Profile.js
+++ src/js/view/Profile.js
@@ -2,19 +2,22 @@ import { section, h1, h2, p, a, ul, li } from '@hyperapp/html'
 
 export default () => state =>
   section([
-    h1('Repositories'), 
+    h1('Links'),
+    h2('FIA'),
+    ul(
+      [
+        { title: 'The complete 2019 F1 Championship calendar| Formula 1®', url: 'https://www.formula1.com/en/racing/2019.html' },
+        { title: 'Calendar - FIA World Endurance Championship', url: 'https://www.fiawec.com/en/calendar/80' }
+      ].map(d =>
+        li([
+          a({ href: d.url, target: '_blank' }, d.title)
+        ])
+      )
+    ),
+
+    h1('Repositories'),
     h2('Program:'),
     a({ href: 'https://github.com/y047aka/MotorSportsCalendar', target: '_blank' }, 'https://github.com/y047aka/MotorSportsCalendar'),
     h2('Data:'),
-    a({ href: 'https://github.com/y047aka/MotorSportsSchedules', target: '_blank' }, 'https://github.com/y047aka/MotorSportsSchedules'),
-    h1('Links'),
-    h2('FIA'),
-    ul([
-      li([
-        a({ href: 'https://www.formula1.com/en/racing/2019.html', target: '_blank' }, 'The complete 2019 F1 Championship calendar| Formula 1®')
-      ]),
-      li([
-        a({ href: 'https://www.fiawec.com/en/calendar/80', target: '_blank' }, 'Calendar - FIA World Endurance Championship')
-      ])
-    ])
+    a({ href: 'https://github.com/y047aka/MotorSportsSchedules', target: '_blank' }, 'https://github.com/y047aka/MotorSportsSchedules')
   ])
