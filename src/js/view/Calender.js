421196fb9355524232a0603e5dc62393e1065af5
戸塚孝高 <y047aka@totsukayoshitakanoMac-mini.local>
2019-02-14 20:37:24 +0900

    array => sundays

--- src/js/view/Calender.js
+++ src/js/view/Calender.js
@@ -1,6 +1,6 @@
 import { span, section, table, tr, th, td } from '@hyperapp/html'
 
-const array = [...Array(365).keys()].map(d => new Date(2019, 0, d + 1)).filter(d => d.getDay() === 0)
+const sundays = [...Array(365).keys()].map(d => new Date(2019, 0, d + 1)).filter(d => d.getDay() === 0)
 
 const check = (sunday, series) => {
   const a = series.races.filter(d => {
@@ -13,7 +13,7 @@ const check = (sunday, series) => {
 const TableHead = () => state =>
   tr([
     th(''),
-    array.map(d => 
+    sundays.map(d => 
       th([
         span(d.getDate() <= 7 ? ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][d.getMonth()] : '')
       ])
@@ -23,7 +23,7 @@ const TableHead = () => state =>
 const TableBody = series => state =>
   tr([
     td(series.seriesName),
-    array.map(d => ([
+    sundays.map(d => ([
       check(d, series) ? td({ class: 'raceweek', 'data-tooltip': '24 Hours of Le Mans' }, '') : td('')
     ]))
   ])
