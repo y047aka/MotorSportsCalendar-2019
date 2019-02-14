bc72db7ed07128435a6f6aeafb2fc794213f11fa
戸塚孝高 <y047aka@totsukayoshitakanoMac-mini.local>
2019-02-15 02:27:13 +0900

    Enable tooltip

--- src/js/view/Calender.js
+++ src/js/view/Calender.js
@@ -2,18 +2,10 @@ import { span, section, table, tr, th, td } from '@hyperapp/html'
 
 const sundays = [...Array(365).keys()].map(d => new Date(2019, 0, d + 1)).filter(d => d.getDay() === 0)
 
-const check = (sunday, series) => {
-  const a = series.races.filter(d => {
-    const difference = sunday - new Date(d.date.replace(/-/g, '/'))
-    return (difference >= 0 && difference < 7 * (1000 * 60 * 60 * 24))
-  }).length
-  return a
-}
-
 const TableHead = () => state =>
   tr([
     th(''),
-    sundays.map(d => 
+    sundays.map(d =>
       th([
         span(d.getDate() <= 7 ? ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][d.getMonth()] : '')
       ])
@@ -23,9 +15,18 @@ const TableHead = () => state =>
 const TableBody = series => state =>
   tr([
     td(series.seriesName),
-    sundays.map(d => ([
-      check(d, series) ? td({ class: 'raceweek', 'data-tooltip': '24 Hours of Le Mans' }, span([d.getDate()])) : td([span(d.getDate())])
-    ]))
+    sundays.map(sunday => {
+      const event = series.races.find(s => {
+        const difference = sunday - new Date(s.date.replace(/-/g, '/'))
+        return (difference >= 0 && difference < 7 * (1000 * 60 * 60 * 24))
+      })
+
+      if (event) {
+        return td({ class: 'raceweek', 'data-tooltip': `${ event.name }` }, span([sunday.getDate()]))
+      } else {
+        return td([span(sunday.getDate())])
+      }
+    })
   ])
 
 export default () => state =>
