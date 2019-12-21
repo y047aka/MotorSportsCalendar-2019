6559547a595b9f2ca0fabbb041191acff71e5d13
Yoshitaka <yoshitaka.totsuka@siiibo.com>
2019-12-22 08:19:28 +0900

    Remove unneccesary API

--- src/elm/Races.elm
+++ src/elm/Races.elm
@@ -1,9 +1,8 @@
-module Races exposing (Race, RaceCategory, getServerResponse, getServerResponseWithCategoryTask)
+module Races exposing (Race, RaceCategory, getServerResponse)
 
 import Http
 import Iso8601
 import Json.Decode as Decode
-import Task
 import Time
 
 
@@ -53,41 +52,3 @@ getServerResponse msg url =
         { url = url
         , expect = Http.expectJson msg raceCategoryDecoder
         }
-
-
-getServerResponseWithCategoryTask : String -> Task.Task Http.Error RaceCategory
-getServerResponseWithCategoryTask fileName =
-    Http.task
-        { method = "GET"
-        , headers = []
-        , url = fileName
-        , body = Http.emptyBody
-        , resolver = jsonResolver raceCategoryDecoder
-        , timeout = Nothing
-        }
-
-
-jsonResolver : Decode.Decoder a -> Http.Resolver Http.Error a
-jsonResolver decoder =
-    Http.stringResolver <|
-        \response ->
-            case response of
-                Http.BadUrl_ url ->
-                    Err (Http.BadUrl url)
-
-                Http.Timeout_ ->
-                    Err Http.Timeout
-
-                Http.NetworkError_ ->
-                    Err Http.NetworkError
-
-                Http.BadStatus_ metadata body ->
-                    Err (Http.BadStatus metadata.statusCode)
-
-                Http.GoodStatus_ metadata body ->
-                    case Decode.decodeString decoder body of
-                        Ok value ->
-                            Ok value
-
-                        Err err ->
-                            Err (Http.BadBody (Decode.errorToString err))
